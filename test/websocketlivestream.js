f72f5daa2a48e387c703cdf0c20fbf8e52b27a1b
holger <wretched@web.de>
2016-04-10 18:52:58 +0200

    Added new tests

--- test/websocketlivestream.js
+++ test/websocketlivestream.js
@@ -1,8 +1,8 @@
 var expect = require('chai').expect;
-var WebSocketLivestream = require('../src/WebSocketLivestream');
-var wsLivestream = new WebSocketLivestream();
+var websocketlivestream = require('../src/websocketlivestream');
+var wsLivestream = new websocketlivestream();
 
-describe('WebSocketLivestream', function()
+describe('websocketlivestream', function()
 {
     var socket1 = {name: 'socket1', url: 'ws://192.168.42.1', port: '8000'};
     var socket2 = {name: 'socket2', url: 'ws://192.168.42.1', port: '8000'};
@@ -67,6 +67,39 @@ describe('WebSocketLivestream', function()
         {
             wsLivestream.addNewSegmentToBroadcastQueue(newSegment);
             expect(wsLivestream.broadcastQueue).to.deep.equal(['filename1']);
+        });
+    });
+
+    describe('removeFirstSegmentInArray', function()
+    {
+        it('removes the first entry from the mpg queue', function()
+        {
+            wsLivestream.addNewSegmentToMpgSegmentQueue('filename2');
+            wsLivestream.removeFirstSegmentInArray('mpgSegmentQueue');
+            expect(wsLivestream.mpgSegmentQueue).to.deep.equal(['filename2']);
+        });
+
+        it('removes the first entry from the mp4 queue', function()
+        {
+            wsLivestream.addNewSegmentToMp4SegmentQueue('filename2');
+            wsLivestream.removeFirstSegmentInArray('mp4SegmentQueue');
+            expect(wsLivestream.mp4SegmentQueue).to.deep.equal(['filename2']);
+        });
+
+        it('removes the first entry from the broadcast queue', function()
+        {
+            wsLivestream.addNewSegmentToBroadcastQueue('filename2');
+            wsLivestream.removeFirstSegmentInArray('broadcastQueue');
+            expect(wsLivestream.broadcastQueue).to.deep.equal(['filename2']);
+        });
+    });
+
+    describe('updateLastBroadcastElement', function()
+    {
+        it('Sets the recently send segment equal to the first element in line', function()
+        {
+            wsLivestream.updateLastBroadcastElement();
+            expect(wsLivestream.lastBroadcastElement).to.be.equal('filename2');
         });
     });
 });
