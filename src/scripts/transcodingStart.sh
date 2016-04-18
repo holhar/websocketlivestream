b8ec5269b0ce8b213573de9fc4c4b9cf9d0e6dc2
holger <wretched@web.de>
2016-04-18 13:53:51 +0200

    Added instance-ID as input argument

--- src/scripts/transcodingStart.sh
+++ src/scripts/transcodingStart.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 # this script starts the transcoding watcher
-# one argument needed:
+# two arguments needed:
 #   "- argument 1 must be 'local' or 'network'"
+#   "- argument 2 is the instance number of the transcoder"
 
-if [ -z $1 ]
+if [ -z $2 ]
 then
-    echo "Missing argument"
-    echo "One argument needed:"
+    echo "Missing arguments"
+    echo "Two arguments needed:"
     echo "- argument 1 must be 'local' or 'network'"
+    echo "- argument 2 is the instance number of the transcoder"
     exit 2
-elif [ $# -gt 3 ]
+elif [ $# -gt 2 ]
 then
     echo "Too many arguments"
-    echo "One argument needed:"
+    echo "Two arguments needed:"
     echo "- argument 1 must be 'local' or 'network'"
+    echo "- argument 2 is the instance number of the transcoder"
     exit 2
 fi
 
-if [ "$1" = "local" -o "$1" = "network" ]
+if [ "$1" = "local" -o "$1" = "network" -a "$2" = "1" -o "$2" = "2" ]
 then
     cd ../data/dashsegments/
-    node transcode.js $1
+    node transcode.js $1 $2
 else
     echo "Wrong arguments provided"
-    echo "One argument needed:"
+    echo "Two arguments needed:"
     echo "- argument 1 must be 'local' or 'network'"
+    echo "- argument 2 is the instance number of the transcoder"
     exit 2
 fi
