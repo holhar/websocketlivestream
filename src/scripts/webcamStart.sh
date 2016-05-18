deed5640037762c2c957d6d5db5f480a9cba4b97
Holger Harms <kontakt@hash-developer.de>
2016-05-18 10:23:00 +0200

    Added framerate and buffer config

--- src/scripts/webcamStart.sh
+++ src/scripts/webcamStart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../data/mpgsegments/
-ffmpeg -f qtkit -i "default" webcamout.mpg -map 0 -f ssegment -segment_list playlist.m3u8 -segment_list_flags +live -segment_time 3 webcam_part%03d.mpg
+ffmpeg -f qtkit -i "default" -r 25 -qscale:v 2 -maxrate 8000k -bufsize 24000k webcamout.mpg -map 0 -f segment -segment_list playlist.m3u8 -segment_list_flags +live -segment_time 3 webcam_part%03d.mpg
